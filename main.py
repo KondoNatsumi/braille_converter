61739a1b7f535377ef1f08e7118c92a1335b99d4
近藤菜摘 <KONDO@kondounatsumi-no-MacBook-Pro.local>
2018-08-11 12:11:08 +0900

    first commit

---
+++ main.py
@@ -0,0 +1,17 @@
+# あ → 点字
+
+a = "● -\n- - \n- -"
+i = "● -\n● - \n- -"
+
+# foo = input("入力してください：")
+
+# print("出力", foo)
+input_value = input("入力してください：")
+
+if input_value == "あ":
+	print(a)
+elif input_value == "い":
+	print(i)
+else:
+	print("ないね")
+
